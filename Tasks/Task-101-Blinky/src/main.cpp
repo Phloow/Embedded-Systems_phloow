89402bf01cdf57399923878e849edbe5514848b6
Shofolarin B Shomefun <pholareeen@gmail.com>
2023-07-29 13:20:28 +0100

    Edited code to alternate three LEDs

--- Tasks/Task-101-Blinky/src/main.cpp
+++ Tasks/Task-101-Blinky/src/main.cpp
@@ -1,13 +1,13 @@
 #include <mbed.h>
-//#include "mbed.h"       // this is relevanyt when programming in the Mbed Studio enviroment.
+//!#include "mbed.h"       // this is relevant when programming in the Mbed Studio enviroment.
 
 // Create a DigitalOut “object” called greenLED. Pass constant LED1 as a “parameter”
 DigitalOut greenLED(LED1);
 
 //These are "commented out" and so are not part of the program. You can uncomment them by removing the // characters
 //Your task is to make a sequence alternating between Green+Red and just Blue 
-//DigitalOut blueLED(LED2);
-//DigitalOut redLED(LED3);
+DigitalOut blueLED(LED2);
+DigitalOut redLED(LED3);
 
 //The main function - all executable C / C++ applications have a main function. This is our entry point in the software
 int main() 
@@ -17,16 +17,32 @@ int main()
     { 
         //The code between the { curly braces } is the code that is repeated forever
 
-        // Turn onboard LED ON  
-        greenLED = 1; 
+        // Turn red LED ON  
+        redLED = 1; 
 
-        // Wait 0.2 second (1 million microseconds)
-        wait_us(500000); 
+        // Wait 1 second (1 million microseconds)
+        wait_us(1000000); 
 
-        // Turn LED OFF
+        // Turn red LED OFF
+        redLED = 0;
+
+        // Turn blue LED ON  
+        blueLED = 1;
+
+        // Wait 1 second
+        wait_us(1000000); 
+
+        // Turn blue LED OFF  
+        blueLED = 0; 
+
+        // Turn green LED ON  
+        greeLED = 1;
+
+        // Wait 1 second
+        wait_us(1000000); 
+
+        // Turn greenLED OFF
         greenLED = 0;
 
-        // Wait 0.2 second
-        wait_us(500000); 
     }
 }
