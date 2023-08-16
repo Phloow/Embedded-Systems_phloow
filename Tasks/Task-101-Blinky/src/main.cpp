0f9bd2cb3ccc309515977133875d96e2d663833f
Shofolarin B Shomefun <pholareeen@gmail.com>
2023-08-16 18:44:24 +0100

    Board change.
    Changed board type from Nucleo F429ZI to Nucleo F401RE in platformio.ini

--- Tasks/Task-101-Blinky/src/main.cpp
+++ Tasks/Task-101-Blinky/src/main.cpp
@@ -1,13 +1,13 @@
 #include <mbed.h>
-//!#include "mbed.h"       // this is relevant when programming in the Mbed Studio enviroment.
+//#include "mbed.h"       // this is relevant when programming in the Mbed Studio enviroment.
 
 // Create a DigitalOut “object” called greenLED. Pass constant LED1 as a “parameter”
 DigitalOut greenLED(LED1);
 
 //These are "commented out" and so are not part of the program. You can uncomment them by removing the // characters
 //Your task is to make a sequence alternating between Green+Red and just Blue 
-DigitalOut blueLED(LED2);
-DigitalOut redLED(LED3);
+//DigitalOut blueLED(LED2);
+//DigitalOut redLED(LED3);
 
 //The main function - all executable C / C++ applications have a main function. This is our entry point in the software
 int main() 
@@ -18,31 +18,66 @@ int main()
         //The code between the { curly braces } is the code that is repeated forever
 
         // Turn red LED ON  
-        redLED = 1; 
+        //redLED = 1; 
 
         // Wait 1 second (1 million microseconds)
-        wait_us(1000000); 
+        //wait_us(1000000); 
 
         // Turn red LED OFF
-        redLED = 0;
+        //redLED = 0;
 
         // Turn blue LED ON  
-        blueLED = 1;
+        //blueLED = 1;
 
         // Wait 1 second
-        wait_us(1000000); 
+        //wait_us(1000000); 
 
         // Turn blue LED OFF  
-        blueLED = 0; 
+        //blueLED = 0; 
+
+        int i = 0;
+        int j = 0;
+
+        while(i < 2){
+            // Turn green LED ON  
+            greenLED = 1;
+
+            // Wait 1 second
+            wait_us(1000000); 
+
+            // Turn greenLED OFF
+            greenLED = 0;
+
+            // Wait 1 second
+            wait_us(1000000);
+            i = i + 1; 
+        }
+
+        while(j < 5) {
+            // Turn green LED ON
+            greenLED = 1;
+
+            // Wait 1 second
+            wait_us(250000);
+
+            // Turn greenLED OFF
+            greenLED = 0;
+
+            // Wait 1 second
+            wait_us(250000);
+            j = j + 1;
+          }
 
         // Turn green LED ON  
-        greenLED = 1;
+        //greenLED = 1;
 
         // Wait 1 second
-        wait_us(1000000); 
+       // wait_us(1000000); 
 
         // Turn greenLED OFF
-        greenLED = 0;
+        //greenLED = 0;
 
+        // Wait 1 second
+        //wait_us(1000000);
     }
 }
