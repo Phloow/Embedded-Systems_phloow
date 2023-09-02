c2fdc8218fd003302bd9262ab06ec6d98a8c0483
Shofolarin B Shomefun <pholareeen@gmail.com>
2023-09-02 16:41:45 +0100

    modified main.cpp

--- Tasks/Task-101-Blinky/src/main.cpp
+++ Tasks/Task-101-Blinky/src/main.cpp
@@ -1,83 +1,46 @@
-//#include <mbed.h>         // If running from PlatformIO, mbed.h must be included as a global file.
-#include "mbed.h"       // this is relevant when programming in the Mbed Studio enviroment.
+/*********
+Program: Basic program to blink the onboard LED on the Nucleo board.
+Target device: Nucleo F401RE or Nucleo F429ZI
+Framework: Mbed OS
+Author: Nicholas Outram (Task 1 from Embedded Systems Course)
+Modified by: Folarin Shomefun (following my template)
+Credits: None
+Code Status: Working perferctly as tested with nucleo boards F401RE and F429ZI from Nick.
+*********/
 
-// Create a DigitalOut “object” called greenLED. Pass constant LED1 as a “parameter”
-DigitalOut greenLED(LED1);
+/*----- Module 1 = INCLUDES (Library & files) -----*/
+#include "mbed.h" //this is relevant to be able to use mbed commands and sysntax.
+/*-----End of Module 1 -----*/
 
-//These are "commented out" and so are not part of the program. You can uncomment them by removing the // characters
-//Your task is to make a sequence alternating between Green+Red and just Blue 
-//DigitalOut blueLED(LED2);
-//DigitalOut redLED(LED3);
+/*----- Module 2 = VARIABLES NAME & TYPE DECLARATION, #DEFINES, VARIABLE INITIALIZATION, PIN DEFINITIONS, LIBRARY INSTATIATION, LIBRARY PORT / SITE ADDRESS ASSIGNMENT, FLAGS, -----*/
+DigitalOut greenLED(PB_0);  //Instantiate the DigitalOut method as a variable called "greenLED" and pass the port we want to control as its parameter. LED 1 is on PB_0 of F429ZI. Unlike Arduino where you have to define a port pin first before setting the mode(OUT or IN), you do this in one go in mbed!
+DigitalOut blueLED(PB_7);  // LED2 port pin on F429ZI 
+DigitalOut redLED(PB_14);   // LED3 port pin on F429ZI
+/*-----End of Module 2 -----*/
 
-//The main function - all executable C / C++ applications have a main function. This is our entry point in the software
-int main() 
-{
-    // ALL the repeating code is contained in a  “while loop”
-    while(true) 
-    { 
-        //The code between the { curly braces } is the code that is repeated forever
+/*----- Module 3 = FUNCTION PRE-DECLARATIONS -----*/ // none to declare
+/*----- Module 4 = SETUP() Start Libraries/modules, set pin modes, start serial, start libraries, start displays, etc  -----*/
+// Mbed OS does not use a setup() function; everything goes into module 2
 
-        // Turn red LED ON  
-        //redLED = 1; 
+/*----- Module 5 = MAIN() -----*/
+int main()
+{      
+    while (true) // Unlike Arduino, where the loop() function automatically repaeats, in Mbed (more C++esque), you have to introduce the while(true) loop yourself.
+    {
+        int i = 0; // iterant definition and initialisation
+        int j = 0; // another iterant definition and initialisation
 
-        // Wait 1 second (1 million microseconds)
-        //wait_us(1000000); 
-
-        // Turn red LED OFF
-        //redLED = 0;
-
-        // Turn blue LED ON  
-        //blueLED = 1;
-
-        // Wait 1 second
-        //wait_us(1000000); 
-
-        // Turn blue LED OFF  
-        //blueLED = 0; 
-
-        int i = 0;
-        int j = 0;
-
-        while(i < 2){
-            // Turn green LED ON  
-            greenLED = 1;
-
-            // Wait 1 second
-            wait_us(1000000); 
-
-            // Turn greenLED OFF
-            greenLED = 0;
-
-            // Wait 1 second
-            wait_us(1000000);
-            i = i + 1; 
+        while (i <= 2) {
+            greenLED = !greenLED;   //invert the state of greenLED, whatever it is, every cycle
+            wait_us(1000000);   // Wait 1 second      
+            i++;
         }
 
-        while(j < 5) {
-            // Turn green LED ON
-            greenLED = 1;
-
-            // Wait 1 second
-            wait_us(250000);
-
-            // Turn greenLED OFF
-            greenLED = 0;
-
-            // Wait 1 second
-            wait_us(250000);
-            j = j + 1;
-          }
-
-        // Turn green LED ON  
-        //greenLED = 1;
-
-        // Wait 1 second
-       // wait_us(1000000); 
-
-        // Turn greenLED OFF
-        //greenLED = 0;
-
-        // Wait 1 second
-        //wait_us(1000000);
+        while (j <= 5) {
+          redLED = !redLED; //invert the state of redLED, whatever it is, every cycle
+          wait_us(250000); // Wait 1/4 second
+          j++;
+        }
     }
 }
+/*-----End of Module 5 -----*/
