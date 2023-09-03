abdf1df22a02da91e9d69fe590d1d3fea5d2a675
Shofolarin B Shomefun <pholareeen@gmail.com>
2023-09-03 22:56:38 +0100

    shared library for Task 103

--- Tasks/Task-101-Blinky/src/main.cpp
+++ Tasks/Task-101-Blinky/src/main.cpp
@@ -3,8 +3,8 @@ Program: Basic program to blink the onboard LED on the Nucleo board.
 Target device: Nucleo F401RE or Nucleo F429ZI
 Framework: Mbed OS
 Author: Nicholas Outram (Task 1 from Embedded Systems Course)
-Modified by: Folarin Shomefun (following my template)
-Credits: None
+Modified by: Folarin Shomefun (using my template)
+Credits: Nicholas Outram
 Code Status: Working perferctly as tested with nucleo boards F401RE and F429ZI from Nick.
 *********/
 
@@ -14,7 +14,7 @@ Code Status: Working perferctly as tested with nucleo boards F401RE and F429ZI f
 
 /*----- Module 2 = VARIABLES NAME & TYPE DECLARATION, #DEFINES, VARIABLE INITIALIZATION, PIN DEFINITIONS, LIBRARY INSTATIATION, LIBRARY PORT / SITE ADDRESS ASSIGNMENT, FLAGS, -----*/
 DigitalOut greenLED(PB_0);  //Instantiate the DigitalOut method as a variable called "greenLED" and pass the port we want to control as its parameter. LED 1 is on PB_0 of F429ZI. Unlike Arduino where you have to define a port pin first before setting the mode(OUT or IN), you do this in one go in mbed!
-DigitalOut blueLED(PB_7);  // LED2 port pin on F429ZI 
+DigitalOut blueLED(PB_7);  // LED2 port pin on F429ZI. You can as well replace PB_0, PB_7 and PB_14 with "LED1", "LED2", and "LED3" which are library defined aliases for these pins 
 DigitalOut redLED(PB_14);   // LED3 port pin on F429ZI
 /*-----End of Module 2 -----*/
 
