52a57b309e5d3d46c8bf70af8539056330b125a7
Shofolarin B Shomefun <pholareeen@gmail.com>
2023-09-11 09:52:48 +0100

    Update main.cpp

--- Tasks/Task-110-Variables/src/main.cpp
+++ Tasks/Task-110-Variables/src/main.cpp
@@ -9,10 +9,11 @@ Code Status:    //!Mbed Studio - Not tested on Mbed Studio (leave 'libuopmsb.lib
 Code Status:    //TODO: PlatformIO - Compiled and working perfectly on F429ZI + MSB
 Notes:          //?For PlatformIO, libuopmsb library folder needed to be copied into the "lib" folder as a local library. There was no need to include "mbed.h" as defining framwork in platformio.ini takes care of this
                 //!COMPILE AT LEAST TWICE!!! - Sometimes, you get an error on compilation the first time. This happens quite often!
+                //!To use the serial port in PlatformIO, you have to instantialte the 'Serial' class to use serial functions such as 'printf()'. This appears to have been done under-the-hood in Mbed Studio such that you can use the functions directly.
 *********/
 
 /*----- Module 1 = INCLUDES (Library & files) -----*/
-//include "mbed.h"  //not necessary because it has already been included in the uop_msb library
+// include "mbed.h"  //not necessary because it has already been included in the uop_msb library
 #include "uop_msb.h"
 using namespace uop_msb; // university of plymouth universal library for the embedded system course
 /*----- End of Module 1 -----*/
@@ -34,6 +35,8 @@ DigitalOut LED_BLUE_LE(LED_BLUE_LE_PIN, 0); // same as above
 DigitalIn BlueButton(USER_BUTTON);
 
 BusOut ledData(LED_D0_PIN, LED_D1_PIN, LED_D2_PIN, LED_D3_PIN, LED_D4_PIN, LED_D5_PIN, LED_D6_PIN, LED_D7_PIN);
+
+Serial nucleo(USBTX, USBRX); // instantiate the "Serial" module as an object 'nucleo'; USBTX -  tx, USBRX - rx
 /*----- End of Module 2 -----*/
 
 /*----- Module 3 = FUNCTION PRE-DECLARATIONS - Only relevant for Arduino framwework -----*/ // none to declare
@@ -42,58 +45,59 @@ BusOut ledData(LED_D0_PIN, LED_D1_PIN, LED_D2_PIN, LED_D3_PIN, LED_D4_PIN, LED_D
 /*----- Module 5 = MAIN() -----*/ // none to declare
 int main()
 {
-    printf("\nTASK-110\n");
+    nucleo.printf("\nTASK-110\n");
 
     // Integer
     int count = 0;
-    printf("count = %d\n", count);
+    nucleo.printf("count = %d\n", count);
 
     // Add one to count
     count = count + 1;
-    printf("New value of count = %d\n", count);
+    nucleo.printf("New value of count = %d\n", count);
 
     // Char
     char character;
     character = 'A';
-    printf("The character %c has the ASCII code %d\n", character, character);
+    nucleo.printf("The character %c has the ASCII code %d\n", character, character);
 
     // Short
     short shortCount = 32766;
-    printf("The value of shortCount is %hd\n", shortCount);
+    nucleo.printf("The value of shortCount is %hd\n", shortCount);
     shortCount = shortCount + 1;
-    printf("add 1 and shortCount is %hd\n", shortCount);
+    nucleo.printf("add 1 and shortCount is %hd\n", shortCount);
 
     // Long long
     long long NN = 0x12345678ABCD0001LL; // Literal in HEX
-    printf("A very large number %lld\n", NN);
-    printf("A very large number in hex %llX\n", NN);
+    nucleo.printf("A very large number %lld\n", NN);
+    nucleo.printf("A very large number in hex %llX\n", NN);
 
     // unsigned
     unsigned short p = 1; // 16 bit
-    printf("unsigned int p = %u\n", p);
+    nucleo.printf("unsigned int p = %u\n", p);
     p = p - 2;
-    printf("Subtract 2. Now unsigned int p = %u\n", p);
+    nucleo.printf("Subtract 2. Now unsigned int p = %u\n", p);
 
     // float
     float pi_float = 3.1415926536;
-    printf("The value of pi is approximately %f\n", pi_float);
+    nucleo.printf("The value of pi is approximately %f\n", pi_float);
 
     // double
     double pi_double = 3.1415926536l;
-    printf("The value of pi is approximately %lf\n", pi_double);
+    nucleo.printf("The value of pi is approximately %lf\n", pi_double);
 
     // Data type sizes (in bytes)
-    printf("Size of a char is %d bytes\n", sizeof(char));
-    printf("Size of a short is %d bytes\n", sizeof(short));
-    printf("Size of a int is %d bytes\n", sizeof(int));
-    printf("Size of a long is %d bytes\n", sizeof(long));
-    printf("Size of a long long is %d bytes\n", sizeof(long long));
-    printf("Size of a float is %d bytes\n", sizeof(float));
-    printf("Size of a double is %d bytes\n", sizeof(double));
+    nucleo.printf("Size of a char is %d bytes\n", sizeof(char));
+    nucleo.printf("Size of a short is %d bytes\n", sizeof(short));
+    nucleo.printf("Size of a int is %d bytes\n", sizeof(int));
+    nucleo.printf("Size of a long is %d bytes\n", sizeof(long));
+    nucleo.printf("Size of a long long is %d bytes\n", sizeof(long long));
+    nucleo.printf("Size of a float is %d bytes\n", sizeof(float));
+    nucleo.printf("Size of a double is %d bytes\n", sizeof(double));
 
     // Stop
-    while (true){
-        printf("This is a test of the serial port!");
+    while (true)
+    {
+        nucleo.printf("This is a test of the serial port!");
     }
 }
 /*-----End of Module 5 -----*/
